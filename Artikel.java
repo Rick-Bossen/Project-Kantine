99abecd3de0109ec03b03299418d43572b955eda
Rick <rickroelof@home.nl>
2019-05-17 00:22:30 +0200

    First commit

---
+++ Artikel.java
@@ -0,0 +1,34 @@
+public class Artikel {
+
+    private String naam;
+    private double prijs;
+
+    /**
+     * Constructor
+     */
+    public Artikel(String naam, double prijs){
+        this.naam = naam;
+        this.prijs = prijs;
+    }
+
+    public Artikel() {
+        naam = null;
+        prijs = 0;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public void setPrijs(double prijs) {
+        this.prijs = prijs;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public double getPrijs() {
+        return prijs;
+    }
+}
